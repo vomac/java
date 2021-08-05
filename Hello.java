1c7d2b895683e9870ede5ee9496dc7d678ad15a7
Kevin <75410454+vomac@users.noreply.github.com>
2021-08-05 17:07:09 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+	  System.out.println("Hello World Java");
+	  	
+	  float f1 = 35e3f;
+	  double d1 = 12E4d;
+	  System.out.println(f1);
+	  System.out.println(d1);
+	
+	}
+ }
